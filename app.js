ca7d13e083e8b95e9f527ebf9b74dbb2cdc45136
Viraj Gupta <viraj24gupta@gmail.com>
2021-11-01 12:42:59 +0530

    templating

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res){
-   res.render('onboarding.ejs')
+    res.render('onboarding.ejs', {title: "OnBoarding"})
 });
 
 app.listen(process.env.PORT,()=>{
