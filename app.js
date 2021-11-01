6785e231acdb11d70bd00ddb61b2a9f1b075ee67
Viraj Gupta <viraj24gupta@gmail.com>
2021-11-01 11:17:51 +0530

    setup

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express')
+const path = require("path");
+let app = express();
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname,'./')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+   res.render('onboarding.ejs')
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log("http://localhost:"+process.env.PORT);
+});
