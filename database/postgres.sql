8327a8bb55e67675c51c96f6172a6149b44c19e1
Viraj Gupta <viraj24gupta@gmail.com>
2021-11-04 10:44:27 +0530

    db dummy data

--- database/postgres.sql
+++ database/postgres.sql
@@ -19,13 +19,34 @@ CREATE TABLE score(
     returns_req int,
     returns_res int,
     ref_count   int,
-    feedback    int
+    feedback    float
 );
 
-create table test(
-    user_id VARCHAR(6) PRIMARY KEY,
-    user_name VARCHAR(255)
-);
+INSERT INTO earnings VALUES ('usr101','121.01','151.01','201.01');
+INSERT INTO earnings VALUES ('usr102','122.02','152.02','202.02');
+INSERT INTO earnings VALUES ('usr103','123.03','153.03','203.03');
+INSERT INTO earnings VALUES ('usr104','124.04','154.04','204.04');
+INSERT INTO earnings VALUES ('usr105','125.05','155.05','205.05');
+
+INSERT INTO location VALUES ('usr101','28.704060','77.102493');
+INSERT INTO location VALUES ('usr102','28.669155','77.453758');
+INSERT INTO location VALUES ('usr103','27.669155','76.453758');
+INSERT INTO location VALUES ('usr104','22.669155','72.453758');
+INSERT INTO location VALUES ('usr105','29.669155','78.453758');
 
 
-drop table test;
+INSERT INTO score VALUES ('usr101','12','11','14','15','4.2');
+INSERT INTO score VALUES ('usr102','2','131','13','14','3');
+INSERT INTO score VALUES ('usr103','122','111','23','35','4.7');
+INSERT INTO score VALUES ('usr104','121','41','24','55','3.6');
+INSERT INTO score VALUES ('usr105','1','14','11','75','1.2');
+
+
+
+-- create table test(
+--     user_id VARCHAR(6) PRIMARY KEY,
+--     user_name VARCHAR(255)
+-- );
+--
+--
+-- drop table test;
